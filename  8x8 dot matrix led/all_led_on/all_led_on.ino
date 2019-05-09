5370b7ef47047ecfdaa374d1719db0afc2993b44
maeng-gu <38423200+maeng-gu@users.noreply.github.com>
2019-05-09 13:04:47 +0900

    Create all_led_on.ino

---
+++  8x8 dot matrix led/all_led_on/all_led_on.ino
@@ -0,0 +1,85 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3,  OUTPUT);
+  pinMode(4,  OUTPUT);
+  pinMode(5,  OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(14, OUTPUT);
+  pinMode(15, OUTPUT);
+  pinMode(16, OUTPUT);
+  pinMode(17, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(2, 1);
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+  digitalWrite(9, 1);
+
+  digitalWrite(10, 0);
+  digitalWrite(11, 0);
+  digitalWrite(12, 0);
+  digitalWrite(13, 0);
+  digitalWrite(14, 0);
+  digitalWrite(15, 0);
+  digitalWrite(16, 0);
+  digitalWrite(17, 0);  
+}
+다 키기
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3,  OUTPUT);
+  pinMode(4,  OUTPUT);
+  pinMode(5,  OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(14, OUTPUT);
+  pinMode(15, OUTPUT);
+  pinMode(16, OUTPUT);
+  pinMode(17, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(2, 1);
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  digitalWrite(7, 1);
+  digitalWrite(8, 1);
+  digitalWrite(9, 1);
+
+  digitalWrite(10, 0);
+  digitalWrite(11, 0);
+  digitalWrite(12, 0);
+  digitalWrite(13, 0);
+  digitalWrite(14, 0);
+  digitalWrite(15, 0);
+  digitalWrite(16, 0);
+  digitalWrite(17, 0);  
+}
