f81576cfab1709194d6923c7ce638a93b7a9f83b
maeng-gu <38423200+maeng-gu@users.noreply.github.com>
2019-06-21 09:36:16 +0900

    .

---
+++ led_on_duty/led_on_8_duty/led_on_8_duty.ino
@@ -0,0 +1,37 @@
+void setup() {
+  
+}
+unsigned long previousMillis = 0; 
+int count, duty;
+
+
+void loop() {
+  unsigned long currentMicros = micros(); 
+   
+  if(currentMicros = p_micros > 100)
+  {
+    previousMillis = currentMicros;
+    
+    count++
+    if(count == 100)
+    {
+      digitalWrite(13, 1);
+      count = 0;
+    }
+    
+    if(count == duty)
+    {
+      digitalWrite(13, 0);
+    }
+      
+  }
+
+  if(currentMicros - previousMillis > 20)
+  {
+    duty++;
+    if(duty == 90)
+    {
+      duty = 10;
+    }
+  }
+}
