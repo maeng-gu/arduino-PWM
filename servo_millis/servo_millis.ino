1bbd75bef32d186b8f8c346d0050cc05133126f7
maeng-gu <38423200+maeng-gu@users.noreply.github.com>
2019-06-27 11:46:10 +0900

    Update servo_millis.ino

--- servo_millis/servo_millis.ino
+++ servo_millis/servo_millis.ino
@@ -1,96 +1,44 @@
 void setup() {
-
-  pinMode(13, OUTPUT);
   pinMode(3, OUTPUT);
   
 }
 unsigned long previousMicors = 0; 
 unsigned long previousMillis = 0; 
-unsigned long previousMicors1 = 0; 
-unsigned long previousMillis1 = 0; 
+
 int count = 0;
-int count1 = 0;
 int duty = 0;
 int duty_servo = 0;
 
 
 void loop() {
   unsigned long currentMicros = micros(); 
-  unsigned long currentMicros_1 = micros(); 
   unsigned long currentMillis = millis(); 
 
-   
-  if(currentMicros - previousMicors > 100)
+  if(currentMicros - previousMicors)
   {
     previousMicors = currentMicros;
-    
     count++;
-    if(count == 100)
+    if(count == 625)
     {
-      digitalWrite(13, 1);
       count = 0;
-    }
-    
-    if(count == duty)
-    {
-      digitalWrite(13, 0);
-    }
-      
-  }
-
-  if(currentMillis - previousMillis > 20)
-  {
-    previousMillis = currentMillis;
-    
-    duty++;
-    if(duty == 90)
-    {
-      duty = 10;
-    }
-  }
-  /////--------------------
-
-  if(currentMicros_1 - previousMicors1)
-  {
-    previousMicors1 = currentMicros_1;
-    count1++;
-    if(count1 == 625)
-    {
-      count1 = 0;
       digitalWrite(3, 1);
     }
     
-    if(count1 == duty_servo)
+    if(count == duty_servo)
     {
       digitalWrite(3, 0);
     }
 
-    if(currentMillis - previousMillis1 > 20)
+    
+    
+  }
+  if(currentMillis - previousMillis > 20)
     {
-      previousMillis1 = currentMillis;
+      previousMillis = currentMillis;
       duty_servo++;
       if(duty_servo == 72)
       {
         duty_servo = 22;
       }
     }
-    ///////////////
-    if(c_mill - p_mill > 10)
-    {
-      flag = 1;
-      p_mill = c_mill;
-      D.W H
-      p_micros = c_micros = micros();
-    }
-
-    if(flag == 1)
-    {
-      if(c_micors - p_micros2 > 100)
-      {
-        p_micors2 = c_micros;
-        DW L
-        flag = 0;
-      }
-    }
-  }
 }
